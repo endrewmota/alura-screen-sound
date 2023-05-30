8185b5dc97de02e4e5f1c6dd62fb842edc928ff9
endrewmota <endrew.desousa@gmail.com>
2023-05-29 20:33:31 -0400

    Creating menu options

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -17,6 +17,11 @@ void ExibirLogo()
 void ExibirOpcoesDoMenu()
 {
     ExibirLogo();
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
 }
 
 ExibirOpcoesDoMenu();
