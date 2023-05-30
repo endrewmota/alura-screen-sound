77055b559701a5bfc98089bf48184e0b755e3f90
endrewmota <endrew.desousa@gmail.com>
2023-05-29 20:46:02 -0400

    Creating option Regostrar Banda

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -29,9 +29,36 @@ void ExibirOpcoesDoMenu()
 
     switch (opcaoEscolhidaNumerica)
     {
-        case 1: Console.WriteLine("\nVocê escolheu a opção 1");
+        case 1: RegistrarBanda();
+            break;
+
+        case 2:
+            Console.WriteLine("\nVocê escolheu a opção 2");
+            break;
+
+        case 3:
+            Console.WriteLine("\nVocê escolheu a opção 3");
+            break;
+
+        case 4:
+            Console.WriteLine("\nVocê escolheu a opção 4");
+            break;
+
+        case -1:
+            Console.WriteLine("\nVocê escolheu a opção -1");
             break;
     }
+}
+
+void RegistrarBanda()
+{
+    Console.Clear();
+    Console.Write("Digite o nome da banda que deseja registrar: ");
+    string nomeDaBanda = Console.ReadLine()!;
+    Console.WriteLine($"Banda {nomeDaBanda} registrada com sucesso!");
+    Thread.Sleep(4000);
+    Console.Clear();
+    ExibirOpcoesDoMenu();
 }
 
 ExibirOpcoesDoMenu();
