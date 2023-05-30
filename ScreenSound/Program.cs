e2278113f470fdb9382c301a67faeb89fbaeef3a
endrewmota <endrew.desousa@gmail.com>
2023-05-29 20:38:23 -0400

    Creating menu option selected

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -22,6 +22,16 @@ void ExibirOpcoesDoMenu()
     Console.WriteLine("Digite 3 para avaliar uma banda");
     Console.WriteLine("Digite 4 para exibir a média de uma banda");
     Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a sua opção:");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    switch (opcaoEscolhidaNumerica)
+    {
+        case 1: Console.WriteLine("\nVocê escolheu a opção 1");
+            break;
+    }
 }
 
 ExibirOpcoesDoMenu();
